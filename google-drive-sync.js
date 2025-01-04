7f5cb632157c716fadffd055612812ad8eb02ef3
Leeingnyo <dyd1928@naver.com>
2025-01-05 01:17:53 +0900

    Implement save, load, remove

--- google-drive-sync.js
+++ google-drive-sync.js
@@ -1,3 +1,8 @@
+// polyfill for BigInt
+if (BigInt) {
+  BigInt.prototype.toJSON = function() { return this.toString(); }
+}
+
 // gapi
 import {
   getFiles,
@@ -44,6 +49,57 @@ export class GoogleDriveSync {
 
     this._google_ready = false;
     this._user_drive_ready = false;
+  }
+
+  #keytype(key) {
+    return `GDS.${key}.type`;
+  }
+
+  #keydata(key) {
+    return `GDS.${key}.data`;
+  }
+
+  load(key) {
+    const type = localStorage.getItem(this.#keytype(key));
+    if (type === null) {
+      return;
+    }
+
+    const data = localStorage.getItem(this.#keydata(key));
+    if (data === null) {
+      return;
+    }
+
+    if (type === 'undefined') {
+      return;
+    } else if (type === 'bigint') {
+      return BigInt(JSON.parse(data));
+    } else if (type === 'number') {
+      return JSON.parse(data);
+    } else if (type === 'string') {
+      return JSON.parse(data);
+    } else if (type === 'boolean') {
+      return JSON.parse(data);
+    } else if (type === 'object') {
+      return JSON.parse(data);
+    }
+  }
+
+  save(key, value) {
+    const type = typeof value;
+    if (type === 'symbol' || type === 'function') { // ignored
+      return;
+    }
+
+    localStorage.setItem(this.#keytype(key), type);
+    // type === 'bigint' // nested bigint is transformed into string
+    localStorage.setItem(this.#keydata(key), JSON.stringify(value));
+    return;
+  }
+
+  remove(key) {
+    localStorage.removeItem(this.#keytype(key));
+    localStorage.removeItem(this.#keydata(key));
   }
 
   /**
